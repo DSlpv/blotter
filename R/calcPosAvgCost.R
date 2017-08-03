abc6ae04d6d29741bd9365963533df06180a8ca2
DSlpv <solopoff@mail.ru>
2017-08-03 21:56:02 +0300

    Update calcPosAvgCost.R

--- R/calcPosAvgCost.R
+++ R/calcPosAvgCost.R
@@ -21,7 +21,7 @@
 }
 
 .calcPosAvgCost_C <- function(PrevPosQty, PrevPosAvgCost, TxnValue, PosQty, ConMult=1)
-    .Call(calcPosAvgCost, PrevPosQty, PrevPosAvgCost, TxnValue, PosQty, ConMult)
+    .Call("calcPosAvgCost", PrevPosQty, PrevPosAvgCost, TxnValue, PosQty, ConMult)
 
 ###############################################################################
 # Blotter: Tools for transaction-oriented trading systems development
